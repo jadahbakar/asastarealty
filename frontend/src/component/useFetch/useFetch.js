dd6ff155c4d5b7ff7d4301058fa2eaaef05e109c
jadahbakar <dedy.styawan@gmail.com>
2020-03-18 10:11:35 +0700

    add comment

--- frontend/src/component/useFetch/useFetch.js
+++ frontend/src/component/useFetch/useFetch.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
+// modif dari useHTTP
 const useHttp = (url, token) => {
   const [fetchedData, setFetchedData] = useState([])
 
